b6bab2504eb432aa5656b467649c0b20fb6870f5
Markus Kuukka <kuukka85@gmail.com>
2022-10-02 23:38:36 +0300

    Tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Delete(index){
+
+
+}
+
+
 function App() {
+
+  const [todo, setTodo] = useState({desc: '', date:''});
+  const [todos, setTodos] = useState([]);
+
+
+  const addTodo = (event) =>{
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const inputChanged = (event) =>{
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const deleteTodo = (index) =>{
+
+    var newTodos = todos; 
+    newTodos.splice(index,1);
+    setTodos([...newTodos]); 
+
+
+
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form onSubmit={addTodo}>
+      <label for="date">Date: </label>
+        <input type="date" name="date" value={todo.date} onChange={inputChanged}/>
+        <label for="desc">Description: </label>
+        <input type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
+        <input type="submit" value="Add" />
+
+     </form>
+    <table>
+      <tbody>
+        <h1>Simple Todolist</h1>
+        <tr><th>Date</th><th>Description</th>
+        </tr>
+    {
+      todos.map((todo, index) =>
+        <tr key={index}>
+            
+            <td>{todo.date}</td>
+            <td>{todo.desc}</td>
+            <button onClick={()=>deleteTodo(index)}>Delete</button>
+        </tr>
+
+      )
+    }
+
+
+
+      </tbody>
+    </table>
+
     </div>
   );
 }
